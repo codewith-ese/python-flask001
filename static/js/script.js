4730be44859b90e66d7a3e8ed1bc571138b9f870
codewith-ese <ese24002@byui.edu>
2024-10-04 16:17:49 +0100

    aaaaa

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,25 @@
-function sendMail(){
-    let parms ={
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value,
-    }
-    emailjs.send("service_4wlqm4f","template_ssgob8i",parms).then(alert("Email Sent!! Successfully"))
-}
+// function sendMail(){
+//     let parms ={
+//         name : document.getElementById("name").value,
+//         email : document.getElementById("email").value,
+//         subject : document.getElementById("subject").value,
+//         message : document.getElementById("message").value,
+//     }
+//     emailjs.send("service_4wlqm4f","template_ssgob8i",parms).then(alert("Email Sent!! Successfully"))
+// }
 
 
 
 
-const openModalBtnElement = document.querySelector('#openModal');
-const modalElement = document.querySelector('.modal');
-const modalContentElement = modalElement.querySelector('.modal_content');
+// const openModalBtnElement = document.querySelector('#openModal');
+// const modalElement = document.querySelector('.modal');
+// const modalContentElement = modalElement.querySelector('.modal_content');
 
-openModalBtnElement.addEventListener('click', () =>{
-    modalElement.classList.add('open')
-});
+// openModalBtnElement.addEventListener('click', () =>{
+//     modalElement.classList.add('open')
+// });
 
-modalContentElement.addEventListener('click', () =>{
-    modalElement.classList.remove('open')
-});
+// modalContentElement.addEventListener('click', () =>{
+//     modalElement.classList.remove('open')
+// });
 
